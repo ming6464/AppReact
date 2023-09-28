5cc9cb3d69bc565533018761f1b441091d4c7932
minhng <minhng@hypersol.vn>
2023-09-28 07:11:25 +0700

    first commiet

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, ToastAndroid, View, Button } from "react-native";
 
 export default function App() {
+  const ShowToast = (text) => {
+    ToastAndroid.showWithGravity(text, ToastAndroid.LONG, ToastAndroid.TOP);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>au fuck</Text>
+      <Button title="Show Toast" onPress={() => ShowToast("Hello")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
